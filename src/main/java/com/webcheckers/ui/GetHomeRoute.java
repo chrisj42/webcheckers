8768738d57681ade714c5120b93b18f7aef68b21
Christopher Johns <cmj9076@rit.edu>
2019-02-26 14:42:11 -0500

    modified whitespace

--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -21,56 +21,56 @@ import spark.TemplateEngine;
  * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
  */
 public class GetHomeRoute implements Route {
-  private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
-
-  private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
-
-  private final TemplateEngine templateEngine;
-
-  /**
-   * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
-   *
-   * @param templateEngine
-   *   the HTML template rendering engine
-   */
-  public GetHomeRoute(final TemplateEngine templateEngine) {
-    this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
-    //
-    LOG.config("GetHomeRoute is initialized.");
-  }
-
-  /**
-   * Render the WebCheckers Home page.
-   *
-   * @param request
-   *   the HTTP request
-   * @param response
-   *   the HTTP response
-   *
-   * @return
-   *   the rendered HTML for the Home page
-   */
-  @Override
-  public Object handle(Request request, Response response) {
-    LOG.finer("GetHomeRoute is invoked.");
-    //
-    Map<String, Object> vm = new HashMap<>();
-    vm.put("title", "Welcome!");
-
-    // display a user message in the Home page
-    vm.put("message", WELCOME_MSG);
-    
-    vm.put("currentUser", new Player("Chris"));
-    vm.put("redPlayer", new Player("Chris"));
-    vm.put("whitePlayer", new Player("Steve"));
-    
-    vm.put("activeColor", Color.RED);
-    
-    vm.put("viewMode", ViewMode.PLAY);
-    
-    vm.put("board", new BoardView());
-
-    // render the View
-    return templateEngine.render(new ModelAndView(vm , "game.ftl"));
-  }
+	private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+	
+	private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
+	
+	private final TemplateEngine templateEngine;
+	
+	/**
+	 * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+	 *
+	 * @param templateEngine
+	 *   the HTML template rendering engine
+	 */
+	public GetHomeRoute(final TemplateEngine templateEngine) {
+		this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+		//
+		LOG.config("GetHomeRoute is initialized.");
+	}
+	
+	/**
+	 * Render the WebCheckers Home page.
+	 *
+	 * @param request
+	 *   the HTTP request
+	 * @param response
+	 *   the HTTP response
+	 *
+	 * @return
+	 *   the rendered HTML for the Home page
+	 */
+	@Override
+	public Object handle(Request request, Response response) {
+		LOG.finer("GetHomeRoute is invoked.");
+		//
+		Map<String, Object> vm = new HashMap<>();
+		vm.put("title", "Welcome!");
+		
+		// display a user message in the Home page
+		vm.put("message", WELCOME_MSG);
+		
+		vm.put("currentUser", new Player("Chris"));
+		vm.put("redPlayer", new Player("Chris"));
+		vm.put("whitePlayer", new Player("Steve"));
+		
+		vm.put("activeColor", Color.RED);
+		
+		vm.put("viewMode", ViewMode.PLAY);
+		
+		vm.put("board", new BoardView());
+		
+		// render the View
+		return templateEngine.render(new ModelAndView(vm , "game.ftl"));
+	}
 }
--- src/main/java/com/webcheckers/util/Message.java
+++ src/main/java/com/webcheckers/util/Message.java
@@ -14,103 +14,103 @@ import java.util.logging.Logger;
  * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
  */
 public final class Message {
-  private static final Logger LOG = Logger.getLogger(Message.class.getName());
-
-  //
-  // Static Factory methods
-  //
-
-  /**
-   * A static helper method to create new error messages.
-   *
-   * @param message  the text of the message
-   *
-   * @return a new {@link Message}
-   */
-  public static Message error(final String message) {
-    return new Message(message, Type.ERROR);
-  }
-
-  /**
-   * A static helper method to create new informational messages.
-   *
-   * @param message  the text of the message
-   *
-   * @return a new {@link Message}
-   */
-  public static Message info(final String message) {
-    return new Message(message, Type.INFO);
-  }
-
-  //
-  // Inner Types
-  //
-
-  /**
-   * The type of {@link Message}; either information or an error.
-   */
-  public enum Type {
-    INFO, ERROR
-  }
-
-  //
-  // Attributes
-  //
-
-  private final String text;
-  private final Type type;
-
-  //
-  // Constructor
-  //
-
-  /**
-   * Create a new message.
-   *
-   * @param message  the text of the message
-   * @param type  the type of message
-   */
-  private Message(final String message, final Type type) {
-    this.text = message;
-    this.type = type;
-    LOG.finer(this + " created.");
-  }
-
-  //
-  // Public methods
-  //
-
-  /**
-   * Get the text of the message.
-   */
-  public String getText() {
-    return text;
-  }
-
-  /**
-   * Get the type of the message.
-   */
-  public Type getType() {
-    return type;
-  }
-
-  /**
-   * Query whether this message was generated from a successful
-   * action; ie, not an {@link Type#ERROR}.
-   *
-   * @return true if not an error
-   */
-  public boolean isSuccessful() {
-    return !type.equals(Type.ERROR);
-  }
-
-  //
-  // Object methods
-  //
-
-  @Override
-  public String toString() {
-    return "{Msg " + type + " '" + text + "'}";
-  }
-
+	private static final Logger LOG = Logger.getLogger(Message.class.getName());
+	
+	//
+	// Static Factory methods
+	//
+	
+	/**
+	 * A static helper method to create new error messages.
+	 *
+	 * @param message  the text of the message
+	 *
+	 * @return a new {@link Message}
+	 */
+	public static Message error(final String message) {
+		return new Message(message, Type.ERROR);
+	}
+	
+	/**
+	 * A static helper method to create new informational messages.
+	 *
+	 * @param message  the text of the message
+	 *
+	 * @return a new {@link Message}
+	 */
+	public static Message info(final String message) {
+		return new Message(message, Type.INFO);
+	}
+	
+	//
+	// Inner Types
+	//
+	
+	/**
+	 * The type of {@link Message}; either information or an error.
+	 */
+	public enum Type {
+		INFO, ERROR
+	}
+	
+	//
+	// Attributes
+	//
+	
+	private final String text;
+	private final Type type;
+	
+	//
+	// Constructor
+	//
+	
+	/**
+	 * Create a new message.
+	 *
+	 * @param message  the text of the message
+	 * @param type  the type of message
+	 */
+	private Message(final String message, final Type type) {
+		this.text = message;
+		this.type = type;
+		LOG.finer(this + " created.");
+	}
+	
+	//
+	// Public methods
+	//
+	
+	/**
+	 * Get the text of the message.
+	 */
+	public String getText() {
+		return text;
+	}
+	
+	/**
+	 * Get the type of the message.
+	 */
+	public Type getType() {
+		return type;
+	}
+	
+	/**
+	 * Query whether this message was generated from a successful
+	 * action; ie, not an {@link Type#ERROR}.
+	 *
+	 * @return true if not an error
+	 */
+	public boolean isSuccessful() {
+		return !type.equals(Type.ERROR);
+	}
+	
+	//
+	// Object methods
+	//
+	
+	@Override
+	public String toString() {
+		return "{Msg " + type + " '" + text + "'}";
+	}
+	
 }
