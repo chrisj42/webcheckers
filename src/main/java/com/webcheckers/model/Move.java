953d226aab6762f5c97408f996cb4d6d378283a2
Christopher Johns <cmj9076@rit.edu>
2019-03-25 18:47:19 -0400

    merge Move and Position classes

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -1,25 +1,36 @@
 package com.webcheckers.model;
 
+import java.util.Objects;
+
 public class Move {
-
-    private Position start;
-
-    private Position end;
-
-
-    public Move(Position start, Position end){
-        this.start = start;
-        this.end = end;
-
-    }
-
-
-    public Position getStart(){
-        return this.start;
-    }
-
-
-    public Position getEnd(){
-        return this.end;
-    }
+	
+	private final Position start;
+	private final Position end;
+	
+	public Move(Position start, Position end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	public Position getStart() {
+		return start;
+	}
+	
+	public Position getEnd() {
+		return end;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(!(o instanceof Move)) return false;
+		Move move = (Move) o;
+		return Objects.equals(start, move.start) &&
+			Objects.equals(end, move.end);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(start, end);
+	}
 }
--- src/main/java/com/webcheckers/model/Position.java
+++ src/main/java/com/webcheckers/model/Position.java
@@ -1,27 +1,36 @@
 package com.webcheckers.model;
 
+import java.util.Objects;
+
 public class Position {
-
-    private int row;
-
-    private int cell;
-
-
-    public Position(int row, int cell){
-        this.row = row;
-        this.cell = cell;
-
-    }
-
-
-    public int getRow(){
-        return this.row;
-
-    }
-
-
-    public int getCell(){
-        return this.cell;
-
-    }
+	
+	private final int row;
+	private final int cell;
+	
+	public Position(int row, int cell) {
+		this.row = row;
+		this.cell = cell;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCell() {
+		return cell;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(!(o instanceof Position)) return false;
+		Position position = (Position) o;
+		return row == position.row &&
+			cell == position.cell;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(row, cell);
+	}
 }
