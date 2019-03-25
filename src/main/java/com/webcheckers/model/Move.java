723d2cd96d5d2e65356903c0b160f4668fe6b884
Steven McLeod <46683189+StevenXM@users.noreply.github.com>
2019-03-25 18:14:20 -0400

    Move and Position class created

---
+++ src/main/java/com/webcheckers/model/Move.java
@@ -0,0 +1,25 @@
+package com.webcheckers.model;
+
+public class Move {
+
+    private Position start;
+
+    private Position end;
+
+
+    public Move(Position start, Position end){
+        this.start = start;
+        this.end = end;
+
+    }
+
+
+    public Position getStart(){
+        return this.start;
+    }
+
+
+    public Position getEnd(){
+        return this.end;
+    }
+}
